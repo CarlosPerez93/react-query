d9b5888030d08c90744c5b8c59cdde94caaa51dc
CarlosPerez93 <Carlos.Perez3@outlook.es>
2023-10-23 15:37:41 +0200

    feat(Api config): Actions and interfaces

--- 03-react-query-store/src/products/index.ts
+++ 03-react-query-store/src/products/index.ts
@@ -1,3 +1,5 @@
+export { productsApi } from "../api/productsApi";
+
 export { ProductCard } from "./components/productCard";
 
 export { StoreLayout } from "./layout/StoreLayout";
@@ -6,3 +8,5 @@ export { CompleteListPage } from "./pages/completeListPage";
 export { MensPage } from "./pages/mensPage";
 export { NewProduct } from "./pages/newProduct";
 export { WomensPage } from "./pages/womensPage/WomenPage";
+
+export type { Product } from "./../interfaces/products";
