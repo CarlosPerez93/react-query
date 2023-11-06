ac47cbdd02a2e339411ec823dcb17cad9def044a
CarlosPerez93 <Carlos.Perez3@outlook.es>
2023-11-06 19:40:15 +0100

    Form(watch): * react-hook-form installed * useForm  and watch with      completed form.

--- 03-react-query-store/src/products/pages/newProduct/newPrduct.type.ts
+++
@@ -1,6 +0,0 @@
-import propTypes from "prop-types";
-
-export const NewProductProps = {};
-export const NewProductDefaultProps = {};
-
-export type NewProductProps = propTypes.InferProps<typeof NewProductProps>;
---
+++ 03-react-query-store/src/products/pages/newProduct/newProduct.type.ts
@@ -0,0 +1,14 @@
+export type NewProductProps = {
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
+export type NewProductDefaultProps = {
+  title: "";
+  description: "";
+  price: 0;
+  category: "";
+  image: "";
+};
