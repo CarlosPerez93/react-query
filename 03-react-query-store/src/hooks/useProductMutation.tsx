476cd4b7273affbc2f788e90cf9f9a4ee3c78cd6
CarlosPerez93 <Carlos.Perez3@outlook.es>
2023-11-07 22:47:54 +0100

    feat(Error):optimistic Error

--- 03-react-query-store/src/hooks/useProductMutation.tsx
+++ 03-react-query-store/src/hooks/useProductMutation.tsx
@@ -43,6 +43,23 @@ export const useProductMutation = () => {
         }
       );
     },
+
+    onError: (error, variables, context) => {
+      console.log({ error, variables, context });
+
+      queryClient.removeQueries(["product", context?.optimisticProduct.id]);
+
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: variables.category }],
+        (old) => {
+          if (!old) return [];
+
+          return old.filter((cacheProduct) => {
+            return cacheProduct.id !== context?.optimisticProduct.id;
+          });
+        }
+      );
+    },
   });
 
   return mutation;
